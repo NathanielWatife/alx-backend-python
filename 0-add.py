1f6ff9da89e4c290e2fb9a0c2ae1849da18fb885
NathanielWatife <awosanminathaniel0@gmail.com>
2024-11-12 01:21:40 +0100

    Async comprehension

--- 0-add.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-"""type-annotated function add that takes a float a and a float b"""
-
-
-def add(a: float, b: float) -> float:
-    """Add two integer togethers."""
-    return a + b
